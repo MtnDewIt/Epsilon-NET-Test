ad80a477b109d77b822d1aae3cb6e8cec0e394a4
unk-1 <unk0x4@gmail.com>
2020-07-19 17:07:51 +0100

    Add mod package metadata editor

---
+++ Epsilon/Plugins/ModPackagePlugin/MetadataEditorViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CacheEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagTool.Cache;
+
+namespace ModPackagePlugin
+{
+    class MetadataEditorViewModel : CacheEditorTool
+    {
+        public const string ToolName = "ModPackage.MetadataEditorTool";
+
+        public MetadataEditorViewModel(ModPackage package)
+        {
+            DisplayName = "Metadata";
+            PreferredLocation = EpsilonLib.Shell.PaneLocation.Right;
+            PreferredWidth = 420;
+            Name = ToolName;
+            Metadata = new MetadataModel(package.Metadata);
+        }
+
+        public MetadataModel Metadata { get; }
+
+        public override bool InitialAutoHidden => true;
+    }
+}
---
+++ Epsilon/Plugins/ModPackagePlugin/MetadataModel.cs
@@ -0,0 +1,45 @@
+﻿using Stylet;
+using TagTool.Cache;
+
+namespace ModPackagePlugin
+{
+    class MetadataModel : PropertyChangedBase
+    {
+        private ModPackageMetadata _metadata;
+
+        public MetadataModel(ModPackageMetadata metadata)
+        {
+            _metadata = metadata;
+        }
+
+        public string Name
+        {
+            get => _metadata.Name;
+            set
+            {
+                _metadata.Name = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public string Author
+        {
+            get => _metadata.Author;
+            set
+            {
+                _metadata.Author = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public string Description
+        {
+            get => _metadata.Description;
+            set
+            {
+                _metadata.Description = value;
+                NotifyOfPropertyChange();
+            }
+        }
+    }
+}
