1fcb66af578e6a2ae1f75c735fb9a5f323cee4b9
Vegetal-Pole <vegetal.halo@gmail.com>
2023-04-24 17:27:03 -0500

    "Add new" button for StringID fields

--- Epsilon/components/TagStructEditor/Fields/StringIdField.cs
+++ Epsilon/components/TagStructEditor/Fields/StringIdField.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TagStructEditor.Common;
 using TagTool.Cache;
 using TagTool.Common;
 
@@ -11,9 +12,13 @@ namespace TagStructEditor.Fields
         public string Value { get; set; }
         public string UnicText { get; set; }
 
+        public bool AddButtonEnabled { get; set; }
+        public DelegateCommand AddStringIDCommand { get; }
+
         public StringIdField(StringTable stringTable, ValueFieldInfo info) : base(info)
         {
             _stringTable = stringTable;
+            AddStringIDCommand = new DelegateCommand(AddNew, () => AddButtonEnabled);
         }
 
         public override void Accept(IFieldVisitor visitor)
@@ -35,15 +40,36 @@ namespace TagStructEditor.Fields
 
         public void OnValueChanged()
         {
+            if (IsPopulating)
+                return;
+
             StringId stringId = StringId.Invalid;
             if (!string.IsNullOrEmpty(Value))
             {
                 stringId = _stringTable.GetStringId(Value);
                 if (string.IsNullOrWhiteSpace(Value) || stringId == StringId.Invalid)
-                   throw new ArgumentException(nameof(Value));
+                {
+                    AddButtonEnabled = true;
+                    AddStringIDCommand.RaiseCanExecuteChanged();
+                    throw new ArgumentException(nameof(Value));
+                }
+                else
+                {
+                    AddButtonEnabled = false;
+                    AddStringIDCommand.RaiseCanExecuteChanged();
+                }
             }
 
             SetActualValue(stringId);
+        }
+
+        private void AddNew()
+        {
+            _stringTable.AddString(Value);
+
+            string stringid = Value;
+            Value = "default";  // resets failed validation appearance
+            Value = stringid;   // set back to added stringid
         }
     }
 }
