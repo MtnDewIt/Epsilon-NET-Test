b5a23dcfbf1bb41625da95f2fa27149843d9b8a0
unk-1 <unk0x4@gmail.com>
2025-09-17 22:33:05 +0100

    Fix issue that prevented opening multiple instances

--- Epsilon/Epsilon/Logging/DefaultLogger.cs
+++ Epsilon/Epsilon/Logging/DefaultLogger.cs
@@ -1,4 +1,5 @@
 ﻿using EpsilonLib.Logging;
+using System;
 using System.Diagnostics;
 using System.IO;
 
@@ -6,21 +7,46 @@ namespace Epsilon.Logging
 {
     class DefaultLogger : ILogHandler
     {
-        const string FileName = "epsilon.log";
-
         private StreamWriter _writer;
 
         public DefaultLogger()
         {
-            _writer = File.AppendText(FileName);
+            OpenLogForWriting();
         }
 
         public void Log(LogMessageType type, string message)
         {
-            var line = $"[{ type.ToString().ToUpper()}]: { message}";
+            var line = $"[{type.ToString().ToUpper()}]: {message}";
             Debug.WriteLine(line);
             _writer.WriteLine(line);
             _writer.Flush();
+        }
+
+        private void OpenLogForWriting()
+        {
+            string logsDir = Directory.CreateDirectory(Path.Combine(AppContext.BaseDirectory, "logs")).FullName;
+            string suffix = "";
+            int attempt = 0;
+            const int maxAttempts = 10;
+
+            while (true)
+            {
+                try
+                {
+
+                    string path = Path.Combine(logsDir, $"epsilon{suffix}.log");
+                    _writer = new StreamWriter(File.Open(path, FileMode.Append, FileAccess.Write, FileShare.Read));
+
+                    break;
+                }
+                catch (IOException ex) when ((uint)ex.HResult == 0x80070020) // ERROR_SHARING_VIOLATION 
+                {
+                    if (++attempt > maxAttempts)
+                        throw new IOException($"Failed to open log file after {maxAttempts} attempts", ex);
+
+                    suffix = $"({attempt})";
+                }
+            }
         }
     }
 }
