7f83aed7b9b31a15ec634cdb6b820ff4c1388eb2
unk-1 <unk0x4@gmail.com>
2025-11-15 04:49:52 +0000

    Track and poke changed fields only

--- Epsilon/Plugins/CacheEditor/RTE/Providers/HaloOnline106708Provider.cs
+++ Epsilon/Plugins/CacheEditor/RTE/Providers/HaloOnline106708Provider.cs
@@ -29,202 +29,27 @@ namespace CacheEditor.RTE.Providers
             return cacheFile.Cache is GameCacheHaloOnlineBase;
         }
 
-        void IRteProvider.PokeTag(IRteTarget target, GameCache cache, CachedTag instance, object definition, ref byte[] RuntimeTagData)
+        private static int ResolveTagIndex(CachedTagHaloOnline instance, GameCacheHaloOnlineBase cache)
         {
-            PokeTag(target, cache, definition, instance as CachedTagHaloOnline, ref RuntimeTagData);
-        }
-
-        private void PokeTag(IRteTarget target, GameCache cache, object definition, CachedTagHaloOnline hoInstance, ref byte[] RuntimeTagDataMap)
-        {
-            var process = Process.GetProcessById((int)target.Id);
-            if (process == null)
-                throw new RteTargetNotAvailableException(this, "Target process could not be found.");
-
-            using (var processStream = new ProcessMemoryStream(process))
+            if(cache is GameCacheModPackage modPak)
             {
-                int tagindex = hoInstance.Index;
-                bool isModPackage = false;
-                //bool isModPackageTag = false;
-                GameCacheModPackage modpak = null;
-                if (cache is GameCacheModPackage)
+                if (modPak.BaseCacheReference.TagCache.TryGetTag(instance.ToString(), out CachedTag baseInstance))
                 {
-                    modpak = (GameCacheModPackage)cache;
-                    isModPackage = true;
+                    return baseInstance.Index;
                 }
-
-                tagindex = ResolveTagIndex(tagindex, isModPackage, modpak);
-                var address = GetTagAddress(processStream, tagindex);
-                if (address == 0)
-                    throw new RteProviderException(this, $"Tag '{hoInstance}' could not be located in the target process.");
-
-                //first get a raw copy of the tag in the cache
-                byte[] tagcachedata;
-                using (var stream = cache.OpenCacheRead())
-                using (var outstream = new MemoryStream())
-                using (EndianWriter writer = new EndianWriter(outstream, EndianFormat.LittleEndian))
+                else
                 {
-                    //deserialize the cache def then reserialize to a stream
-                    var cachedef = cache.Deserialize(stream, hoInstance);
-                    var dataContext = new DataSerializationContext(writer);
-                    cache.Serializer.Serialize(dataContext, cachedef);
-                    StreamUtil.Align(outstream, 0x10);
-                    tagcachedata = outstream.ToArray();
-                }
-
-                //then serialize the current version of the tag in the editor
-                byte[] editordata;
-                using (MemoryStream stream = new MemoryStream())
-                using (EndianWriter writer = new EndianWriter(stream, EndianFormat.LittleEndian))
-                {
-                    var dataContext = new DataSerializationContext(writer);
-                    cache.Serializer.Serialize(dataContext, definition);
-                    StreamUtil.Align(stream, 0x10);
-                    editordata = stream.ToArray();
-                }
-
-                //length should make to make sure the serializer is consistent
-                if (tagcachedata.Length != editordata.Length)
-                {
-                    throw new RteProviderException(this, $"Error: tag size changed or the serializer failed!");
-                }
-
-                //some very rare tags have a size that doesn't match our serialized version, need to fix root cause
-                if (!isModPackage && tagcachedata.Length != hoInstance.TotalSize - hoInstance.CalculateHeaderSize())
-                {
-                    throw new RteProviderException(this, $"Sorry can't poke this specific tag yet (only happens with very rare specific tags), go bug a dev");
-                }
-
-                //pause the process during poking to prevent race conditions
-                Stopwatch stopWatch = new Stopwatch();
-                stopWatch.Start();
-                process.Suspend();
-
-                uint currenttotalsize = 0;
-                uint headersize = 0;
-                List<uint> TagReferenceFixups = new List<uint>();
-                using (EndianReader reader = new EndianReader(processStream))
-                {
-                    CachedTagHaloOnline runtimetag = new CachedTagHaloOnline();
-                    processStream.Seek(address + 4, SeekOrigin.Begin);
-                    ReadHeaderValues(reader, ref currenttotalsize, ref headersize, ref TagReferenceFixups);
-                }
-
-                byte[] CurrentRuntimeTagData = new byte[currenttotalsize - headersize];
-                processStream.Seek(address + headersize, SeekOrigin.Begin);
-                processStream.Read(CurrentRuntimeTagData, 0, (int)(currenttotalsize - headersize));
-
-                if (currenttotalsize - headersize != tagcachedata.Length)
-                {
-                    process.Resume();
-                    throw new RteProviderException(this, $"Error: Loaded tag size did not match cache tag size. Is this tag overwritten in a modpak? Is your modpak built on the most up to date cache?");
-                }
-
-                //Store the process data before the first poke so we know which values are runtime values
-                if (RuntimeTagDataMap.Length == 0)
-                {
-                    RuntimeTagDataMap = new byte[CurrentRuntimeTagData.Length];
-                    for(var i = 0; i < CurrentRuntimeTagData.Length; i++)
+                    int paktagcount = 0;
+                    for (var i = 0; i < instance.Index; i++)
                     {
-                        //this will serve as a map of the tag data, with 1 being pokeable fields, and 0 being nonpokeable
-                        if (CurrentRuntimeTagData[i] == tagcachedata[i])
-                            RuntimeTagDataMap[i] = 1;
+                        if (!cache.TagCacheGenHO.Tags[i].IsEmpty())
+                            paktagcount++;
                     }
+                    return 0xFFFE - paktagcount;
                 }
-
-                if (tagcachedata.Length != RuntimeTagDataMap.Length)
-                {
-                    process.Resume();
-                    throw new RteProviderException(this, $"Error: Loaded tag has changed size since initial poke! Try closing and reopening the tag.");
-                }               
-
-                //write diffed bytes only
-                int patchedbytes = 0;
-                for (var i = 0; i < tagcachedata.Length; i++)
-                {
-                    //patch anything that isn't a runtime modified field
-                    if(RuntimeTagDataMap[i] == 1)
-                    {
-                        CurrentRuntimeTagData[i] = editordata[i];
-                        patchedbytes++;
-                    }
-                }
-
-                //fixup modpak tagrefs
-                #if DEBUG
-                if (isModPackage)
-                {
-                    //a list of all modpak tags in the modpak that are not basecache tags
-                    List<int> modpaktagindices = new List<int>();
-                    for (var i = 0; i < modpak.TagCache.Count; i++)
-                        if (modpak.TagCache.TryGetCachedTag(i, out var taginstance) && !((CachedTagHaloOnline)taginstance).IsEmpty())
-                            modpaktagindices.Add(i);
-
-                    foreach (uint tagreffixup in TagReferenceFixups)
-                    {
-                        int editortagref = BitConverter.ToInt32(editordata, (int)(tagreffixup - headersize));
-
-                        if (modpaktagindices.Contains(editortagref))
-                        {
-                            //find the index of our desired tag in relation to all modpak tags in the modpak that are not basecache tags
-                            int paktagcount = modpaktagindices.Count(x => x < editortagref);
-                            editortagref = 0xFFFE - paktagcount;
-
-                            byte[] newvalue = BitConverter.GetBytes(editortagref);
-                            newvalue.CopyTo(CurrentRuntimeTagData, tagreffixup - headersize);
-                        }
-                    }
-                }
-                #endif
-
-                processStream.Seek(address + headersize, SeekOrigin.Begin);
-                processStream.Write(CurrentRuntimeTagData, 0, CurrentRuntimeTagData.Length);
-                processStream.Flush();
-
-                process.Resume();
-                stopWatch.Stop();
-
-                Console.WriteLine($"Patched {patchedbytes} bytes in {stopWatch.ElapsedMilliseconds / 1000.0f} seconds");
             }
-        }
-
-        private static int ResolveTagIndex(int tagindex, bool isModPackage, GameCacheModPackage modpak)
-        {
-#if DEBUG
-            if (isModPackage && !modpak.BaseCacheReference.TagCache.TryGetCachedTag(tagindex, out var baseTag))
-            {
-                int paktagcount = 0;
-                for (var i = 0; i < tagindex; i++)
-                    if (modpak.TagCache.TryGetCachedTag(i, out var taginstance) && !((CachedTagHaloOnline)taginstance).IsEmpty())
-                        paktagcount++;
-                tagindex = 0xFFFE - paktagcount;
-                //isModPackageTag = true;
-            }
-#endif
-
-            return tagindex;
-        }
-
-        private void ReadHeaderValues(EndianReader reader, ref uint currenttotalsize, ref uint headersize, ref List<uint> TagReferenceFixups)
-        {
-            currenttotalsize = reader.ReadUInt32();
-            var numDependencies = reader.ReadInt16();              // 0x08 int16  dependencies count
-            var numDataFixups = reader.ReadInt16();                // 0x0A int16  data fixup count
-            var numResourceFixups = reader.ReadInt16();            // 0x0C int16  resource fixup count
-            var numTagReferenceFixups = reader.ReadInt16();        // 0x0E int16  tag reference fixup count(was padding)
-
-            reader.BaseStream.Position += 20; //skip to dependencies section
-            reader.BaseStream.Position += 4 * numDependencies; //skip dependencies
-            reader.BaseStream.Position += 4 * numDataFixups; //skip datafixups
-            reader.BaseStream.Position += 4 * numResourceFixups; //skip resourcefixups
-            for (var i = 0; i < numTagReferenceFixups; i++)
-                TagReferenceFixups.Add(reader.ReadUInt32() - 0x40000000);
-
-            headersize = CalculateMemoryHeaderSize(0x24, numDependencies, numDataFixups, numResourceFixups, numTagReferenceFixups);
-        }
-        public uint CalculateMemoryHeaderSize(int TagHeaderSize, int numDependencies, int numDataFixups, int numResourceFixups, int numTagReferenceFixups)
-        {
-            var size = (uint)(TagHeaderSize + numDependencies * 4 + numDataFixups * 4 + numResourceFixups * 4 + numTagReferenceFixups * 4);
-            return (uint)((size + 0xF) & ~0xF);  // align to 0x10
+          
+            return instance.Index;           
         }
 
         private static uint GetTagAddress(ProcessMemoryStream stream, int tagIndex)
@@ -255,14 +80,6 @@ namespace CacheEditor.RTE.Providers
             return reader.ReadUInt32();
         }
 
-        public void DumpData(string filename, byte[] data)
-        {
-            using (var fs = new FileStream(filename, FileMode.Create, FileAccess.Write))
-            {
-                fs.Write(data, 0, data.Length);
-            }
-        }
-
         public ProcessMemoryStream CreateStream(IRteTarget target)
         {
             var process = Process.GetProcessById((int)target.Id);
@@ -274,15 +91,7 @@ namespace CacheEditor.RTE.Providers
 
         public long GetTagMemoryAddress(ProcessMemoryStream stream, GameCache cache, CachedTag instance)
         {
-            GameCacheModPackage modpak = null;
-            bool isModPackage = false;
-            if (cache is GameCacheModPackage)
-            {
-                modpak = (GameCacheModPackage)cache;
-                isModPackage = true;
-            }
-
-            return GetTagAddress(stream, ResolveTagIndex(instance.Index, isModPackage, modpak));
+            return GetTagAddress(stream, ResolveTagIndex((CachedTagHaloOnline)instance, (GameCacheHaloOnlineBase)cache));
         }
     }
 }
